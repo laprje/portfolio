15d70e2d8df2738dd1f28dc6a19979442c4100d0
L J <laprje@gmail.com>
2019-12-11 15:02:37 -0700

    first portfolio commit

--- server.js
+++ server.js
@@ -19,24 +19,24 @@ var transporter = nodeMailer.createTransport({
     }
 });
 
-app.post("/api/contact", (req, res) => {
-    let {name, email, message} = req.body;
-    console.log(name, email, message)
-    const mailOptions = {
-        from: 'Portfolio Contact',
-        to: 'jshmccann@gmail.com',
-        subject: `${name} reached out from your portfolio!`,
-        html: `<p style='font-family: Sans-serif; color: #bada55; background-color: grey; font-size: 14px; text-align: center'>${message}<br>Name: ${name}<br>Email: ${email}</p>`
-    }
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(err){
-            console.log(err)
-        } else {
-            console.log(info)
-        }
-    })
-    res.status(200).redirect('https://www.josh-mccann.com')
-})
+// app.post("/api/contact", (req, res) => {
+//     let {name, email, message} = req.body;
+//     console.log(name, email, message)
+//     const mailOptions = {
+//         from: 'Portfolio Contact',
+//         to: 'jshmccann@gmail.com',
+//         subject: `${name} reached out from your portfolio!`,
+//         html: `<p style='font-family: Sans-serif; color: #bada55; background-color: grey; font-size: 14px; text-align: center'>${message}<br>Name: ${name}<br>Email: ${email}</p>`
+//     }
+//     transporter.sendMail(mailOptions, (err, info) => {
+//         if(err){
+//             console.log(err)
+//         } else {
+//             console.log(info)
+//         }
+//     })
+//     res.status(200).redirect('https://www.josh-mccann.com')
+// })
 
 app.listen(4545, () => {
     console.log("Listening on port 4545")
